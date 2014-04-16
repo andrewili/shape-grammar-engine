d86e4441e036721cee773785be60a009581ca192
andrewili <i@andrew.li>
2014-04-16 16:51:53 -0400

    Controller. Changes to abandon

--- rule_steps_model.py
+++ rule_steps_model.py
@@ -14,6 +14,7 @@ class Model(object):
             'c': empty_lshape,
             'c_prime': empty_lshape
         }
+        self.rule = rule.Rule.new_empty()       #   no such thing
 
 if __name__ == '__main__':
     import doctest
